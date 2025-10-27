8be1b3be485c9d2a5885df87c021e4536aa61ae2
Ramfis <ramchireno@gmail.com>
2025-10-27 20:21:44 +0100

    Produccion

---
+++ Acceso directo.bat
@@ -0,0 +1,7 @@
+start "" "C:\Program Files\nodejs\node.exe" "C:\Users\5toE-0159\Desktop\Alpha_Stock-Inventario-Sistema_WEB\Backend\server.js"
+
+@echo off
+REM Ruta completa de Chrome
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --app=http://localhost:3000/login.html --window-size=1200,800
+exit
+    
